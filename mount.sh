760481f748f340fa841295292d894068ffb4e9ed
SahrulDXD <58771530+SahrulDXD@users.noreply.github.com>
2020-11-27 15:14:57 +0700

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -4,4 +4,4 @@ sudo parted /dev/nvme1n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
 sudo mkfs.xfs /dev/nvme1n1p1
 sudo partprobe /dev/nvme1n1p1
 sudo mount /dev/nvme1n1p1 /srv/daemon-data
-echo "UUID=${UUID_MOUNT   /srv/daemon-data   xfs   defaults,nofail   1   2" >> test.sh
+echo "UUID=${UUID_MOUNT}   /srv/daemon-data   xfs   defaults,nofail   1   2" >> /etc/fstab
