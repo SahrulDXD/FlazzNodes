bc42333cdba3b0d5559858bc5fdd9240f2ed4a10
SahrulDXD <58771530+SahrulDXD@users.noreply.github.com>
2020-11-27 16:36:32 +0700

    Update update-daemon.sh

--- update-daemon.sh
+++ update-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /srv/daemon
-curl -s https://github.com/SahrulDXD/FlazzNodes/raw/main/Daemon.zip
+wget https://github.com/SahrulDXD/FlazzNodes/raw/main/Daemon.zip
 unzip -o Daemon.zip
