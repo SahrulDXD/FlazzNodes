6a53d9867918722532853112791ab4d0307c0b52
SahrulDXD <58771530+SahrulDXD@users.noreply.github.com>
2020-11-27 17:40:42 +0700

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo parted /dev/nvme1n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
 sudo mkfs.xfs /dev/nvme1n1p1
+sleep 5
 sudo partprobe /dev/nvme1n1p1
 sudo mount /dev/nvme1n1p1 /srv/daemon-data
 UUID_MOUNT=$(blkid /dev/nvme1n1p1 -s UUID -o value);
