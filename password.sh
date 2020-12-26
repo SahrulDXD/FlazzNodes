9f11cbb3ad070808dcb1d3d2132ae3b6c985c649
SahrulDXD <58771530+SahrulDXD@users.noreply.github.com>
2020-12-26 16:03:17 +0700

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,1 +1,6 @@
-
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+sudo systemctl reload sshd
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo passwd
+sudo service ssh restart
