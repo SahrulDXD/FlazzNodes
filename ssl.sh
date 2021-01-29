03cfe2838e97fe7a2b22d880786ba6c4829d3bab
SahrulDXD <58771530+SahrulDXD@users.noreply.github.com>
2021-01-30 03:21:35 +0700

    Create ssl.sh

---
+++ ssl.sh
@@ -0,0 +1,37 @@
+OS="ubuntu"
+FQDN="flazznodes.me"
+main() {
+  FAILED=false
+  
+
+  # Install certbot
+  case "$OS" in
+    debian | ubuntu)
+      apt-get -y install certbot python3-certbot-nginx
+      ;;
+    centos)
+      [ "$OS_VER_MAJOR" == "7" ] && yum -y -q install certbot python-certbot-nginx
+      [ "$OS_VER_MAJOR" == "8" ] && dnf -y -q install certbot python3-certbot-nginx
+      ;;
+  esac
+
+  # Obtain certificate
+  certbot --nginx --redirect --no-eff-email --email "$email" -d "$FQDN" || FAILED=true
+
+  # Check if it succeded
+  if [ ! -d "/etc/letsencrypt/live/$FQDN/" ] || [ "$FAILED" == true ]; then
+    print_warning "The process of obtaining a Let's Encrypt certificate failed!"
+    echo -n "* Still assume SSL? (y/N): "
+    read -r CONFIGURE_SSL
+
+    if [[ "$CONFIGURE_SSL" =~ [Yy] ]]; then
+      ASSUME_SSL=true
+      CONFIGURE_LETSENCRYPT=false
+      configure_nginx
+    else
+      ASSUME_SSL=false
+      CONFIGURE_LETSENCRYPT=false
+    fi
+  fi
+}
+main
